22d008704329f93992b7c3c9f76a8e60ebd9bd70
azizmousa <azizmousa1010@gmail.com>
2020-12-20 01:39:40 +0200

    implement the push method

--- include/stack/Stack.hpp
+++ include/stack/Stack.hpp
@@ -12,7 +12,7 @@ private:
     size_t size;
 
 public:
-
+    typedef std::shared_ptr<Node<T>> NodePointer;
     Stack():top{nullptr}, size{0}{}
     ~Stack() = default;
 
@@ -21,6 +21,24 @@ public:
     T get_top()const;
     size_t get_size()const;
 };
+
+
+/*
+ * void Stack<T>::push(T value)
+ * insert a value to the stack
+*/
+template<typename T>
+void Stack<T>::push(T value){
+    NodePointer node = std::make_shared<Node<T>>(value);
+    if(this->size == 0){
+        this->top = node;
+    }else{
+        node->next_ptr = this->top;
+        this->top = node;
+    }
+    this->size++;
+}
+
 
 
 #endif //_STACK_HPP_Z_
