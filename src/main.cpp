0faf40abcd8a11133d21a7a9f04bb2f7513e398b
azizmousa <azizmousa1010@gmail.com>
2020-12-20 02:10:04 +0200

    the main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+#include "stack/Stack.hpp"
+
 int main(int argc, char *argv[]){
+    Stack<int> s;
+    s.push(10);
+    s.push(20);
+    s.pop();
+    std::cout << s.get_top() << std::endl;
+    std::cout << s.get_size() << std::endl;
     return 0;
 }
